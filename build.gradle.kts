f1e2c1ae83a8dde293bdc907e398962a87839468
moshNpink420 <etaabarki.420@gmail.com>
2026-08-31 00:58:03 +0600

    Upgrade Android and Kotlin plugin versions
    Updated Android and Kotlin plugin versions in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.moshnpink420.tradingsignalapp"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.moshnpink420.tradingsignalapp"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
