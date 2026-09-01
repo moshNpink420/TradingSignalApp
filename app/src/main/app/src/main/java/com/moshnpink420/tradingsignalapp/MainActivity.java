2d5545f14cd9487ee799b90dee84cc8709f92716
moshNpink420 <etaabarki.420@gmail.com>
2026-09-01 13:22:03 +0600

    Add MainActivity for Trading Signal App

---
+++ app/src/main/app/src/main/java/com/moshnpink420/tradingsignalapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.moshnpink420.tradingsignalapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Trading Signal App\n\nApp is working!");
+        text.setTextSize(24);
+        text.setPadding(40, 80, 40, 40);
+
+        setContentView(text);
+    }
+}
