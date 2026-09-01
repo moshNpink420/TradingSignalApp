941ed32fd73490d401274648a6cb10a04355c28e
moshNpink420 <etaabarki.420@gmail.com>
2026-09-01 12:53:00 +0600

    Update MainActivity to use TextView for display
    Replaced the layout with a TextView displaying a message.

--- app/src/main/app/src/main/java/com/moshnpink420/tradingsignalapp/MainActivity.kt
+++ app/src/main/app/src/main/java/com/moshnpink420/tradingsignalapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.moshnpink420.tradingsignalapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val textView = TextView(this)
+        textView.text = "Trading Signal App\n\nApp is working!"
+        textView.textSize = 24f
+        textView.setPadding(40, 80, 40, 40)
+
+        setContentView(textView)
     }
 }
